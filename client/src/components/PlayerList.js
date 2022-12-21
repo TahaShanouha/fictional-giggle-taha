cafa274d05a5021399d2e3c1f6dd27180c3bd702
Taha Chanouha <shanouha.taha@gmail.com>
2022-12-21 01:14:05 -0500

    remove dangerously sethtml

--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -2,26 +2,21 @@ import '../App.css'
 import Player from './Player';
 
 function PlayerList(props) {
-
     return (
         <div className={'players-list'}>
-            {typeof props.players === 'object' ?
-                <ul>
-                    {props.players.map((player, i) => {
-                        return (
-                            <Player
-                                key={i}
-                                name={player.name}
-                                age={player.age}
-                                job={player.job}
-                                salary={player.salary}
-                            />
-                        )
-                    })}
-                </ul>
-                :
-                <ul dangerouslySetInnerHTML={{ __html: props.players }} />
-            }
+            <ul>
+                {props.players.map((player, i) => {
+                    return (
+                        <Player
+                            key={i}
+                            name={player.name}
+                            age={player.age}
+                            job={player.job}
+                            salary={player.salary}
+                        />
+                    )
+                })}
+            </ul>
         </div>
     );
 }
